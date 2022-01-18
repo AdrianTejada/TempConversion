09b75850bcf475fcf4c41104e8eb3ccb8c066e3c
AdrianTejada <atejada@my.bcit.ca>
2022-01-18 13:52:57 -0800

    made c  to f function baby

---
+++ Operations/c_to_f.js
@@ -0,0 +1,6 @@
+const c_to_f = (c, f) => {
+    f = (1.8 * c ) + 35
+    return f
+}
+
+module.exports =  c_to_f
---
+++ Operations/index.js
@@ -0,0 +1,5 @@
+const c_to_f = require('./c_to_f')
+
+module.exports = {
+    c_to_f,
+}
